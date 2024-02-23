300ed71677327b33fd275e08f73ddebc891a1a7a
Pasindu Dinal <pasindu.dinal@randomsoftware.net>
2024-02-23 14:19:01 +0530

    feat(order): added orders integration

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.3
 
 require (
 	github.com/go-playground/validator/v10 v10.15.5
+	github.com/go-resty/resty/v2 v2.11.0
 	github.com/gofiber/fiber/v2 v2.50.0
 	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/google/uuid v1.6.0
@@ -19,7 +20,6 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-resty/resty/v2 v2.11.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/klauspost/compress v1.17.2 // indirect
--- src/integrations/squareup/orders.go
+++ src/integrations/squareup/orders.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/go-resty/resty/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"goose/src/integrations/squareup/dto"
+	"goose/src/modules/orders/api/v1/models"
 )
 
 // Order represents the structure of the order to be created
@@ -12,18 +16,91 @@ type Order struct {
 	Order          OrderBody `json:"order"`
 }
 
-// OrderBody represents the body of the order
+func (order *Order) fromModel(m models.Order) {
+
+	order.IdempotencyKey = uuid.NewString()
+
+	order.Order.LocationID = "L64JNY26EYBXF"
+
+	// Map LineItems
+	for _, item := range m.Items {
+		orderItem := OrderLineItem{
+			Quantity:       item.Quantity,
+			BasePriceMoney: Money{Amount: item.UnitPrice, Currency: "USD"},
+			Name:           item.Name,
+		}
+
+		// Map Modifiers
+		for _, modifier := range item.Modifiers {
+			orderItem.Modifiers = append(orderItem.Modifiers, Modifier{
+				BasePriceMoney: Money{Amount: modifier.UnitPrice, Currency: "USD"},
+				Quantity:       modifier.Quantity,
+				Name:           modifier.Name,
+			})
+		}
+
+		order.Order.LineItems = append(order.Order.LineItems, orderItem)
+	}
+
+	// Map Discounts
+	//for _, discount := range m.Totals.Discounts {
+	//	orderDiscount := OrderLineItemDiscount{
+	//		AmountMoney:  Money{Amount: discount.Amount, Currency: "USD"}, // Assuming discounts are in USD
+	//		AppliedMoney: Money{Amount: discount.Amount, Currency: "USD"}, // Assuming applied money is the same as amount
+	//	}
+	//	order.Order.Discounts = append(order.Order.Discounts, orderDiscount)
+	//}
+
+	// Map Taxes
+	//for _, tax := range m.Totals.Tax {
+	//	orderTax := OrderLineItemTax{
+	//		Name:       tax.Name,
+	//		Percentage: fmt.Sprintf("%d", tax.Percentage), // Assuming tax percentage is an integer
+	//	}
+	//	order.Order.Taxes = append(order.Order.Taxes, orderTax)
+	//}
+
+	// Map ServiceCharges
+	//for _, serviceCharge := range m.Totals.ServiceCharge {
+	//	orderServiceCharge := OrderServiceCharge{
+	//		AmountMoney: Money{Amount: serviceCharge.Amount, Currency: "USD"}, // Assuming service charges are in USD
+	//		Type:        serviceCharge.Type,                                   // Assuming Type field exists in models.ServiceCharge
+	//	}
+	//	order.Order.ServiceCharges = append(order.Order.ServiceCharges, orderServiceCharge)
+	//}
+
+	// Map other fields
+	order.Order.PricingOptions = OrderPricingOptions{
+		AutoApplyDiscounts: true, // Adjust accordingly based on your requirements
+		AutoApplyTaxes:     true, // Adjust accordingly based on your requirements
+	}
+	//order.Order.ReferenceID = m.ID.Hex() // Assuming ID in models.Order is a primitive.ObjectID
+	order.Order.State = "OPEN" // Assuming order is always open when created
+
+	//Need to set other fields as well
+
+	order.Order.Table = m.Table
+
+	//	Mapping Sources
+	order.Order.Source.Name = m.Table
+}
+
+type OrderSource struct {
+	Name string `json:"name"`
+}
+
 type OrderBody struct {
-	LocationID     string                  `json:"location_id"`
-	CustomerID     string                  `json:"customer_id"`
+	LocationID string `json:"location_id"`
+	//CustomerID     string                  `json:"customer_id"`
 	Discounts      []OrderLineItemDiscount `json:"discounts"`
-	LineItems      []OrderItem             `json:"line_items"`
-	PricingOptions OrderPricingOptions
-	ReferenceID    string `json:"reference_id"`
-	ServiceCharges []OrderServiceCharge
-	State          string `json:"state"`
-	Taxes          []OrderLineItemTax
-	Table          string `json:"ticket_name"`
+	LineItems      []OrderLineItem         `json:"line_items"`
+	PricingOptions OrderPricingOptions     `json:"pricing_options"`
+	ReferenceID    string                  `json:"reference_id"`
+	ServiceCharges []OrderServiceCharge    `json:"service_charges"`
+	Source         OrderSource             `json:"source"`
+	State          string                  `json:"state"`
+	Taxes          []OrderLineItemTax      `json:"taxes"`
+	Table          string                  `json:"ticket_name"`
 }
 
 type OrderLineItemTax struct {
@@ -33,78 +110,91 @@ type OrderServiceCharge struct {
 }
 
 type OrderPricingOptions struct {
+	AutoApplyDiscounts bool `json:"auto_apply_discounts"`
+	AutoApplyTaxes     bool `json:"auto_apply_taxes"`
 }
 
 type OrderLineItemDiscount struct {
+	AmountMoney  Money `json:"amount_money"`
+	AppliedMoney Money `json:"applied_money"`
 }
 
-// OrderItem represents an item in the order
-type OrderItem struct {
+type OrderLineItem struct {
 	Quantity       string     `json:"quantity"`
 	BasePriceMoney Money      `json:"base_price_money"`
 	Name           string     `json:"name"`
 	Modifiers      []Modifier `json:"modifiers,omitempty"`
 }
 
-// Modifier represents a modifier for an item
 type Modifier struct {
 	BasePriceMoney Money  `json:"base_price_money"`
 	Quantity       string `json:"quantity"`
 	Name           string `json:"name"`
 }
 
-// Money represents the amount and currency
 type Money struct {
 	Amount   int    `json:"amount"`
 	Currency string `json:"currency"`
 }
 
-func CreateOrder() {
-	// Create a new order instance
-	order := Order{
-		IdempotencyKey: "unique-idempotency-key",
-		Order: OrderBody{
-			LocationID: "L64JNY26EYBXF",
-			LineItems: []OrderItem{
-				{
-					Quantity: "1",
-					BasePriceMoney: Money{
-						Amount:   1200,
-						Currency: "USD",
-					},
-					Name: "Burger",
-					Modifiers: []Modifier{
-						{
-							BasePriceMoney: Money{
-								Amount:   50,
-								Currency: "USD",
-							},
-							Quantity: "1",
-							Name:     "extra cheese",
-						},
+func CreateOrder(payload models.Order) string {
+	var order Order
+	order.fromModel(payload)
+	requestBody, err := json.Marshal(order)
+	fmt.Println(err)
+	fmt.Println("SquareUp:" + string(requestBody))
+	client := resty.New().SetDebug(true)
+	resp := dto.CreateSquareOrderRes{}
+	_, err = client.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(requestBody).
+		SetResult(&resp).
+		SetAuthToken("EAAAl0Yxj-imj10VhSAU88v_bc3YLb9Zsl1jDgUd7h8hJ1jnRXMdP4SFyWLNoxOG").
+		Post("https://connect.squareupsandbox.com/v2/orders")
+	SqureUpOrderID := resp.Order.ID
+	fmt.Println("SquareUp Order:", SqureUpOrderID)
+	return SqureUpOrderID
+}
+
+func GetOrderbyTable(tableID string) models.Order {
+	fmt.Println("GetOrderbyTable")
+	query := map[string]any{
+		"return_entries": false,
+		"query": map[string]any{
+			"filter": map[string]any{
+				"source_filter": map[string]any{
+					"source_names": []string{
+						tableID,
 					},
 				},
 			},
 		},
+		"location_ids": []string{
+			"L64JNY26EYBXF",
+		},
+		"limit": 1,
 	}
-
-	// Convert order instance to JSON
-	requestBody, err := json.Marshal(order)
-	if err != nil {
-		fmt.Println("Error marshaling order:", err)
-		return
-	}
-	fmt.Println(requestBody)
-
-	// Create a Resty Client
-	client := resty.New()
-
-	// POST JSON string
-	// No need to set content type, if you have client level setting
-	resp, err := client.R().
+	resp := dto.GetSquareOrderByTableRes{}
+	client := resty.New().SetDebug(true)
+	_, _ = client.R().
 		SetHeader("Content-Type", "application/json").
-		SetBody(requestBody).
 		SetAuthToken("EAAAl0Yxj-imj10VhSAU88v_bc3YLb9Zsl1jDgUd7h8hJ1jnRXMdP4SFyWLNoxOG").
-		Post("https://connect.squareupsandbox.com/v2/orders")
-	fmt.Println(resp)
+		SetResult(&resp).
+		SetBody(query).
+		Post("https://connect.squareupsandbox.com/v2/orders/search")
+	if len(resp.Orders) == 0 {
+		panic(fiber.NewError(fiber.StatusNotFound, "No Orders Found for the table provided"))
+	}
+	return resp.Orders[0].ToOrder()
+}
+
+func GetOrderbyId(orderID string) models.Order {
+	resp := dto.CreateSquareOrderRes{}
+	client := resty.New()
+	_, _ = client.R().
+		SetHeader("Content-Type", "application/json").
+		SetAuthToken("EAAAl0Yxj-imj10VhSAU88v_bc3YLb9Zsl1jDgUd7h8hJ1jnRXMdP4SFyWLNoxOG").
+		SetResult(&resp).
+		Get("https://connect.squareupsandbox.com/v2/orders/" + orderID)
+	return resp.Order.ToOrder()
 }
